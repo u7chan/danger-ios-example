c632c63cd0e838b11adf9bacdc3a1228231980cd
una <unagamix@gmail.com>
2022-08-31 22:29:10 +0900

    initial

---
+++ DangerExample/DangerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerExampleApp.swift
+//  DangerExample
+//
+//  Created by unagami on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct DangerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
