52473da4ccb8df163fbec5d206b8b9f9ace06f26
una <unagamix@gmail.com>
2022-09-03 23:49:09 +0900

    feat: add SwiftLint

---
+++ scripts/run-lint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if ! which mint >/dev/null; then
+    echo 'warning: Mint not installed. Please install mint from https://github.com/yonaskolb/Mint'
+    exit 1
+fi
+
+mint run --silent SwiftLint >swiftlint.result.xml
